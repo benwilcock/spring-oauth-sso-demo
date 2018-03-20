2de7042dc7ec665141cc4e13b79986253734fc14
benwilcock <benwilcock@gmail.com>
2018-03-20 13:51:45 +0000

    dual authentication providers for GitHub and Google. Just needs secrets.

--- src/main/java/com/benwilcock/springoauthssodemo/SecurityController.java
+++ src/main/java/com/benwilcock/springoauthssodemo/SecurityController.java
@@ -8,15 +8,15 @@ import java.security.Principal;
 @RestController
 public class SecurityController {
 
-    /**
-     * Note:
-     * It’s not a great idea to return a whole Principal in a /user endpoint like this
-     * (it might contain information you would rather not reveal to a browser client).
-     * We only did it to get something working quickly. Later in the guide we will convert
-     * the endpoint to hide the information we don’t need the browser to have.
-     * */
+//    @GetMapping({ "/user", "/me" })
+//    public Map<String, String> user(Principal principal) {
+//        Map<String, String> map = new LinkedHashMap<>();
+//        map.put("name", principal.getName());
+//        return map;
+//    }
+
     @GetMapping("/user")
-    public Principal user(Principal principal){
+    public Principal user(Principal principal) {
         return principal;
     }
 }
--- src/main/java/com/benwilcock/springoauthssodemo/SocialWebSecurityConfigurerAdapterImpl.java
+++ src/main/java/com/benwilcock/springoauthssodemo/SocialWebSecurityConfigurerAdapterImpl.java
@@ -1,15 +1,14 @@
 package com.benwilcock.springoauthssodemo;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Component;
@@ -18,6 +17,7 @@ import org.springframework.web.filter.CompositeFilter;
 import javax.servlet.Filter;
 import java.util.ArrayList;
 import java.util.List;
+
 
 @EnableOAuth2Client
 @Component
@@ -52,41 +52,21 @@ public class SocialWebSecurityConfigurerAdapterImpl extends WebSecurityConfigure
                     // ...the user must be authenticated.
                     .authenticated()
                 .and()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+                .and()
                     .logout()
                     .logoutSuccessUrl("/")
                     .permitAll()
+                .and()
+                    .formLogin()
+                    .loginPage("/index.html")
                 .and()
                     .csrf()
                     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
                     .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
-
     }
-
-//    private Filter ssoFilter(){
-//
-//        CompositeFilter filter = new CompositeFilter();
-//        List<Filter> filters = new ArrayList<>();
-//
-//        OAuth2ClientAuthenticationProcessingFilter githubFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/github");
-//        OAuth2RestTemplate githubTemplate = new OAuth2RestTemplate(github, oAuth2ClientContext);
-//        githubFilter.setRestTemplate(githubTemplate);
-//        UserInfoTokenServices githubTokenServices = new UserInfoTokenServices(githubResource.getUserInfoUri(), github.getClientId());
-//        githubTokenServices.setRestTemplate(githubTemplate);
-//        githubFilter.setTokenServices(githubTokenServices);
-//        filters.add(githubFilter);
-//
-//        OAuth2ClientAuthenticationProcessingFilter googleFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/google");
-//        OAuth2RestTemplate googleTemplate = new OAuth2RestTemplate(google, oAuth2ClientContext);
-//        googleFilter.setRestTemplate(googleTemplate);
-//        UserInfoTokenServices googleTokenServices = new UserInfoTokenServices(googleResource.getUserInfoUri(), google.getClientId());
-//        googleTokenServices.setRestTemplate(googleTemplate);
-//        googleFilter.setTokenServices(googleTokenServices);
-//        filters.add(googleFilter);
-//
-//        filter.setFilters(filters);
-//        return filter;
-//    }
 
     private Filter ssoFilter() {
         CompositeFilter filter = new CompositeFilter();
