d5c9810f9d4a9956c88b1f68ef7baa11d2387f51
benwilcock <benwilcock@gmail.com>
2018-03-20 16:34:20 +0000

    Optimised the imports to remove the unused ones.

--- src/main/java/com/benwilcock/springoauthssodemo/controllers/UserController.java
+++ src/main/java/com/benwilcock/springoauthssodemo/controllers/UserController.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
